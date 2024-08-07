8f3b73208f2d8626a3c7c193da0888a0b4693190
yusef carr <ycarr99@gmail.com>
2024-08-07 16:31:14 -0400

    new push

---
+++ app.js
@@ -0,0 +1,26 @@
+var myDog;
+var myCat;
+
+function getSum (a,b){
+var sum = (a + b);
+console.log(sum);
+}
+getSum(3,7);
+
+function getDifference (a,b){
+var difference = (a - b);
+console.log(Difference);
+}
+getDifference(3,7);
+
+function getProduct (a,b){
+var product = (a * b);
+console.log(product);
+}
+getProduct(3,7);
+
+function getDivision (a,b){
+var division = (a / b);
+console.log(division);
+}
+getDivision(3,7);
